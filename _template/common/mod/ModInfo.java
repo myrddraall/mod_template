1b3a2cd0cd3e7911324c74566f15c3c188838872
Myrddraall <Erd@Dark-Nexus>
2013-04-11 20:41:44 -0400

    Fixed package pointer

--- _template/common/mod/ModInfo.java
+++ _template/common/mod/ModInfo.java
@@ -6,8 +6,8 @@ public final class ModInfo
 
     public static final String MOD_ID = "@MOD_ID@";
     public static final String MOD_NAME = "@MOD_NAME@";
-    public static final String SERVER = "@PACKAGE@.proxy.CommonProxy";
-    public static final String CLIENT = "@PACKAGE@.proxy.ClientProxy";
+    public static final String SERVER = "@PACKAGE@.mod.proxy.CommonProxy";
+    public static final String CLIENT = "@PACKAGE@.mod.proxy.ClientProxy";
     public static final boolean CLIENT_REQUIRED = true;
     public static final boolean SERVER_REQUIRED = false;
 
